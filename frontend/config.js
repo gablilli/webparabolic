2d5ad72b0aefeb5c27c73889fb44cf1f4922d4ce
gablilli <167802854+gablilli@users.noreply.github.com>
2025-07-02 12:31:36 +0200

    vite 1

---
+++ frontend/config.js
@@ -0,0 +1,16 @@
+{
+  "name": "yt-webapp-frontend",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "svelte": "^4.0.0"
+  },
+  "devDependencies": {
+    "@sveltejs/vite-plugin-svelte": "^2.0.0",
+    "vite": "^4.0.0"
+  }
+}
